fa24991901e08a4bfaccfd4e14554c69b6ea7adc
Diego Jacobs <diegojacobs9595@gmail.com>
2017-05-29 15:05:19 -0600

    Checking valid movements

--- Static/JS/Services/intelligence.service.js
+++ Static/JS/Services/intelligence.service.js
@@ -17,43 +17,43 @@
         //Functions
         service.minimax = minimax;
 
-        function minimax(board, playerColor, playingColor, depth, max, min) {
+        function minimax(board, evaluatingColor, currentColor, depth, max, min) {
 
             if (depth >= constantsService.MaxDepth || !utilService.hasEmptySpaces(board)) {
-                return { value: (utilService.countPieces(board)[playingColor]), movement: undefined };
+                return { value: (utilService.countPieces(board)[currentColor]), movement: undefined };
             }
 
             // get all valid moves
-            var validMoves = movesService.legalMovements(board, playingColor);
+            var validMoves = movesService.legalMovements(board, currentColor);
 
             var newBoards = [];
             // get all possible states from legal moves
             for (var i = 0; i < validMoves.length; i++) {
                 var move = validMoves[i];
-                var next = utilService.getNewBoard(board, playingColor, move);
+                var next = utilService.getNewBoard(board, currentColor, move);
                 newBoards.push({ next: next, movement: move });
             }
 
             if (validMoves === null || validMoves.length === 0) {
-                return { value: (utilService.countPieces(board)[playingColor]), movement: undefined };
+                return { value: (utilService.countPieces(board)[currentColor]), movement: undefined };
             }
 
-            var nextPlayingColor = playingColor === constantsService.Black ? constantsService.White : constantsService.Black;
+            var nextColor = currentColor === constantsService.Black ? constantsService.White : constantsService.Black;
 
             // maximize 
-            if (playerColor === playingColor)
-                return maxValue(newBoards, playerColor, nextPlayingColor, depth, max, min);
+            if (evaluatingColor === currentColor)
+                return maxValue(newBoards, evaluatingColor, nextColor, depth, max, min);
             else //minimize
-                return minValue(newBoards, playerColor, nextPlayingColor, depth, max, min);
+                return minValue(newBoards, evaluatingColor, nextColor, depth, max, min);
         }
 
-        function maxValue(newBoards, playerColor, playingColor, depth, max, min) {
+        function maxValue(newBoards, evaluatingColor, currentColor, depth, max, min) {
             var v = -Infinity;
 
             for (var i = 0; i < newBoards.length; i++) {
                 var board = newBoards[i];
 
-                v = Math.max(v, minimax(board.next, playerColor, playingColor, depth + 1, max, min).value);
+                v = Math.max(v, minimax(board.next, evaluatingColor, currentColor, depth + 1, max, min).value);
                 max = Math.max(max, v);
                 if (min <= max) {
                     return { value: v, movement: board.movement };
@@ -63,13 +63,13 @@
             return { value: v, movement: newBoards[0].movement };
         }
 
-        function minValue(newBoards, evaluatingColor, playingColor, depth, max, min) {
+        function minValue(newBoards, evaluatingColor, currentColor, depth, max, min) {
             var v = Infinity;
 
             for (var i = 0; i < newBoards.length; i++) {
                 var board = newBoards[i];
 
-                v = Math.min(v, minimax(board.next, evaluatingColor, playingColor, depth + 1, max, min).value);
+                v = Math.min(v, minimax(board.next, evaluatingColor, currentColor, depth + 1, max, min).value);
                 min = Math.min(min, v);
                 if (min <= max) {
                     return { value: v, movement: board.movement };
